da93651cf186eb5b60899cc768dace3d6bbf1a82
徐冰 <38274876+forestsheep911@users.noreply.github.com>
2021-05-13 17:19:35 +0800

    初步完成popup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 /* eslint-disable no-console */
 import './app.scss'
 import $ from 'jquery'
+import Swal from 'sweetalert2'
 
-kintone.events.on(['app.record.create.show', 'app.record.edit.show'], (event) => {
-  kintone.app.record.setFieldShown('field_code_tjd', false)
-  const EleInsertRecommand = kintone.app.record.getSpaceElement('insert_space')
-  console.log(EleInsertRecommand)
-  EleInsertRecommand.innerHTML = `<div class="inliner"></div>
+const htmlRecommend = `<div class="inliner"></div>
   <div class="inlined">
     <!-- Start component -->
     <div class="wk">
@@ -18,7 +15,7 @@ kintone.events.on(['app.record.create.show', 'app.record.edit.show'], (event) =>
       <div class="track">
         <span class="progress"></span>
       </div>
-      <ol class="progress-points" data-current="1">
+      <ul class="progress-points" data-current="4">
         <li class="progress-point">
           <span class="label"></span>
         </li>
@@ -51,48 +48,66 @@ kintone.events.on(['app.record.create.show', 'app.record.edit.show'], (event) =>
         </li>
       </ol>
     </div>
-    <!-- End component -->
-    <!-- Demo only -->
   </div>
   `
-  const $pointArr = $('.progress-point')
-  const $progress = $('.progress').first()
-  const max = $pointArr.length - 1
-  const $points = $('.progress-points').first()
-  const val = +$points.data('current') - 1
 
-  let active
-  let tracker
+kintone.events.on(['app.record.create.show', 'app.record.edit.show'], (event) => {
+  kintone.app.record.setFieldShown('fd_smile', false)
+  kintone.app.record.setFieldShown('fd_recommend', false)
+  const eleKara = kintone.app.record.getSpaceElement('kara')
+  console.log(eleKara)
+  const eleBtn = document.createElement('button')
+  eleBtn.innerText = '开始评价'
+  eleBtn.onclick = () => {
+    Swal.fire({
+      icon: 'question',
+      title: '如果要推荐给朋友，您的推荐度是？',
+      html: htmlRecommend,
+      width: '80%',
+      didOpen: () => {
+        const $pointArr = $('.progress-point')
+        const $progress = $('.progress').first()
+        const max = $pointArr.length - 1
+        const $points = $('.progress-points').first()
+        const val = +$points.data('current') - 1
 
-  tracker = 0
-  active = 0
+        let active
+        let tracker
 
-  function activate(index) {
-    if (index !== active) {
-      active = index
-      const $activeEle = $pointArr.eq(active)
-      $pointArr.removeClass('completed active').slice(0, active).addClass('completed')
-      $activeEle.addClass('active')
-      return $progress.css('width', `${(index / max) * 100}%`)
-    }
-    return $progress
+        tracker = 0
+        active = 0
+
+        function activate(index) {
+          if (index !== active) {
+            active = index
+            const $activeEle = $pointArr.eq(active)
+            $pointArr.removeClass('completed active').slice(0, active).addClass('completed')
+            $activeEle.addClass('active')
+            return $progress.css('width', `${(index / max) * 100}%`)
+          }
+          return $progress
+        }
+
+        $points.on('click', 'li', function pf() {
+          const indexNum = $pointArr.index(this)
+          const zjpd = indexNum === val ? 0 : tracker
+          tracker = indexNum === 0 ? 1 : zjpd
+          console.log(event.record.fd_recommend)
+          const record0 = kintone.app.record.get()
+          record0.record.fd_recommend.value = indexNum + 1
+          kintone.app.record.set(record0)
+          return activate(indexNum)
+        })
+
+        setTimeout(function st() {
+          const thisRecord = kintone.app.record.get()
+          return activate(thisRecord.record.fd_recommend.value - 1)
+        }, 1000)
+      },
+    })
   }
 
-  $points.on('click', 'li', function pf() {
-    const indexNum = $pointArr.index(this)
-    const zjpd = indexNum === val ? 0 : tracker
-    tracker = indexNum === 0 ? 1 : zjpd
-    console.log(event.record.field_code_tjd)
-    const record0 = kintone.app.record.get()
-    record0.record.field_code_tjd.value = indexNum + 1
-    kintone.app.record.set(record0)
-    return activate(indexNum)
-  })
-
-  setTimeout(function st() {
-    const thisRecord = kintone.app.record.get()
-    return activate(thisRecord.record.field_code_tjd.value - 1)
-  }, 1000)
+  eleKara.appendChild(eleBtn)
 
   return event
 })
