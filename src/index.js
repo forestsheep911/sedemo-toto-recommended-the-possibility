c503375c37ae77057ce97167fe23dea676aa848d
徐冰 <38274876+forestsheep911@users.noreply.github.com>
2021-05-20 17:43:23 +0800

    初版提交

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
-import $ from 'jquery'
-import './app.scss'
 import './smile.css'
+import './recommend.css'
 import Swal from 'sweetalert2'
 import img0 from '../img/0.png'
 import img1 from '../img/1.png'
@@ -12,75 +11,75 @@ import img4 from '../img/4.png'
 const htmlSmile = `
 <div class='outter'>
   <div class="icon-border" value=1>
-    <img id="sm0">
+    <img id="sm0"><br>
+    <div>1</div>
   </div>
   <div class="icon-border" value=2>
-    <img id="sm1">
+    <img id="sm1"><br>
+    <div>2</div>
   </div>
   <div class="icon-border" value=3>
-    <img id="sm2">
+    <img id="sm2"><br>
+    <div>3</div>
   </div>
   <div class="icon-border" value=4>
-    <img id="sm3">
+    <img id="sm3"><br>
+    <div>4</div>
   </div>
   <div class="icon-border" value=5>
-    <img id="sm4">
+    <img id="sm4"><br>
+    <div>5</div>
   </div>
 </div>`
 const htmlRecommend = `
-<div class="inliner"></div>
-<div class="inlined">
-  <!-- Start component -->
-  <div class="wk">
-    <div class="baoyuan">抱怨</div>
-    <div class="zhongli">中立</div>
-    <div class="tuijian">推荐</div>
+<div class='outter'>
+  <div class="number-border" value=0>
+    <div>0</div>
   </div>
-  <div class="progress-meter">
-    <div class="track">
-      <span class="progress"></span>
-    </div>
-    <ul class="progress-points" data-current="4">
-      <li class="progress-point">
-        <span class="label"></span>
-      </li>
-      <li class="progress-point">
-        <span class="label"></span>
-      </li>
-      <li class="progress-point">
-        <span class="label"></span>
-      </li>
-      <li class="progress-point">
-        <span class="label"></span>
-      </li>
-      <li class="progress-point">
-        <span class="label"></span>
-      </li>
-      <li class="progress-point">
-        <span class="label"></span>
-      </li>
-      <li class="progress-point">
-        <span class="label"></span>
-      </li>
-      <li class="progress-point">
-        <span class="label"></span>
-      </li>
-      <li class="progress-point">
-        <span class="label"></span>
-      </li>
-      <li class="progress-point">
-        <span class="label"></span>
-      </li>
-    </ol>
+  <div class="number-border" value=1>
+    <div>1</div>
+  </div>
+  <div class="number-border" value=2>
+    <div>2</div>
+  </div>
+  <div class="number-border" value=3>
+    <div>3</div>
+  </div>
+  <div class="number-border" value=4>
+    <div>4</div>
+  </div>
+  <div class="number-border" value=5>
+    <div>5</div>
+  </div>
+  <div class="number-border" value=6>
+    <div>6</div>
+  </div>
+  <div class="number-border" value=7>
+    <div>7</div>
+  </div>
+  <div class="number-border" value=8>
+    <div>8</div>
+  </div>
+  <div class="number-border" value=9>
+    <div>9</div>
+  </div>
+  <div class="number-border" value=10>
+    <div>10</div>
   </div>
 </div>
 `
 
-kintone.events.on(['app.record.create.show', 'app.record.edit.show'], (event) => {
+kintone.events.on(['app.record.detail.show'], (event) => {
+  const recordParm = {}
+  recordParm.app = kintone.app.getId()
+  recordParm.id = kintone.app.record.getId()
+  recordParm.record = {
+    fd_smile: {},
+    fd_recommend: {},
+  }
   kintone.app.record.setFieldShown('fd_smile', false)
   kintone.app.record.setFieldShown('fd_recommend', false)
   const eleKara = kintone.app.record.getSpaceElement('kara')
-  console.log(eleKara)
   const eleBtn = document.createElement('button')
   eleBtn.innerText = '开始评价'
   eleBtn.onclick = () => {
@@ -91,7 +90,6 @@ kintone.events.on(['app.record.create.show', 'app.record.edit.show'], (event) =>
       width: '80%',
       confirmButtonText: 'Next',
       didOpen: () => {
-        console.log('第一个dialog显示了')
         const sm0 = document.querySelector('#sm0')
         sm0.src = img0
         const sm1 = document.querySelector('#sm1')
@@ -103,8 +101,9 @@ kintone.events.on(['app.record.create.show', 'app.record.edit.show'], (event) =>
         const sm4 = document.querySelector('#sm4')
         sm4.src = img4
         const imgs = document.querySelectorAll('.icon-border')
-        imgs.forEach(function (el) {
-          el.onclick = function () {
+        imgs.forEach((el) => {
+          const elcp = el
+          elcp.onclick = function selectSmile() {
             imgs.forEach((el2) => {
               if (el.isEqualNode(el2)) {
                 if (this.classList.contains('selected')) {
@@ -117,11 +116,22 @@ kintone.events.on(['app.record.create.show', 'app.record.edit.show'], (event) =>
               }
             })
             const selectedItem = document.querySelector('.selected')
-            const kinRecord = kintone.app.record.get()
-            kinRecord.record.fd_smile.value = selectedItem.getAttribute('value')
-            kintone.app.record.set(kinRecord)
+            // const kinRecord = kintone.app.record.get()
+            // kinRecord.record.fd_smile.value = selectedItem.getAttribute('value')
+            recordParm.record.fd_smile = {
+              value: selectedItem.getAttribute('value'),
+            }
+            // kintone.app.record.set(kinRecord)
           }
         })
+      },
+      preConfirm: () => {
+        console.log('confirming')
+        if (document.querySelector('.selected')) {
+          return true
+        }
+        Swal.getTitle().textContent = '错误！请先选择满意度。'
+        return false
       },
     }).then((result) => {
       if (result.isConfirmed) {
@@ -132,53 +142,58 @@ kintone.events.on(['app.record.create.show', 'app.record.edit.show'], (event) =>
           width: '80%',
           confirmButtonText: '提交',
           didOpen: () => {
-            console.log('第一个dialog显示了')
-            const $pointArr = $('.progress-point')
-            const $progress = $('.progress').first()
-            const max = $pointArr.length - 1
-            const $points = $('.progress-points').first()
-            const val = +$points.data('current') - 1
-            let active
-            let tracker
-            tracker = 0
-            active = 0
-            function activate(index) {
-              if (index !== active) {
-                active = index
-                const $activeEle = $pointArr.eq(active)
-                $pointArr.removeClass('completed active').slice(0, active).addClass('completed')
-                $activeEle.addClass('active')
-                return $progress.css('width', `${(index / max) * 100}%`)
+            const recommendNumber = document.querySelectorAll('.number-border')
+            recommendNumber.forEach((el) => {
+              const elcp = el
+              elcp.onclick = function selectSmile() {
+                recommendNumber.forEach((el2) => {
+                  if (el.isEqualNode(el2)) {
+                    if (this.classList.contains('selected')) {
+                      this.classList.remove('selected')
+                    } else {
+                      this.classList.add('selected')
+                    }
+                  } else {
+                    el2.classList.remove('selected')
+                  }
+                })
+                const selectedItem = document.querySelector('.selected')
+                recordParm.record.fd_recommend = {
+                  value: selectedItem.getAttribute('value'),
+                }
               }
-              return $progress
-            }
-
-            $points.on('click', 'li', function pf() {
-              const indexNum = $pointArr.index(this)
-              const zjpd = indexNum === val ? 0 : tracker
-              tracker = indexNum === 0 ? 1 : zjpd
-              console.log(event.record.fd_recommend)
-              const kinRecord = kintone.app.record.get()
-              kinRecord.record.fd_recommend.value = indexNum + 1
-              kintone.app.record.set(kinRecord)
-              return activate(indexNum)
             })
-
-            setTimeout(function st() {
-              const thisRecord = kintone.app.record.get()
-              return activate(thisRecord.record.fd_recommend.value - 1)
-            }, 1000)
+          },
+          preConfirm: () => {
+            console.log('confirming')
+            if (document.querySelector('.selected')) {
+              return true
+            }
+            Swal.getTitle().textContent = '错误！请先选择推荐度。'
+            return false
           },
         }).then((result0) => {
           if (result0.isConfirmed) {
-            eleBtn.innerText = '本次已评价，点击重新评价'
+            // console.log(recordParm)
+            kintone.api(kintone.api.url('/k/v1/record', true), 'PUT', recordParm).then(
+              function rp() {
+                eleBtn.disabled = true
+                eleBtn.innerText = '已评价'
+              },
+              function er() {},
+            )
           }
         })
       }
     })
   }
-
   eleKara.appendChild(eleBtn)
+  return event
+})
 
+kintone.events.on(['app.record.create.show', 'app.record.edit.show'], (event) => {
+  const { record } = event
+  record.fd_recommend.disabled = true
+  record.fd_smile.disabled = true
   return event
 })
